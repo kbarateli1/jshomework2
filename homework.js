f6fc8f5befea435a960499d62fa79e4acc4011d8
kbarateli <90282190+kbarateli@users.noreply.github.com>
2022-02-23 16:36:02 +0400

    homework 2

--- homework.js
+++ homework.js
@@ -17,17 +17,77 @@
     // 10, 50, 6, 7, 8, 11, 6, 3, 9
     
 
-    function first (...number){
-        let numberssum = 0;
-        for(let item of number){   
-            numberssum = numberssum +item;
-        }
-            return numberssum;
-        }
+    //     function kate (...args){
+    //     let numberssum = 0;
+    //     for(let item of args){   
+    //         numberssum = numberssum +item;
+    //         console.log(numberssum);
+    // `` }
+    //     return numberssum;
+    //  }
+    //     let result =  kate(10, 50, 6, 7, 8, 11, 6, 3, 9);
+    //      console.log(result);
 
-        first(10, 50, 6, 7, 8, 11, 6, 3, 9);
-        // let result= list(10, 50, 6, 7, 8, 11, 6, 3, 9);
-        // console.log(result);
+
+
+        //  function kate (...args) {
+        //      let numberssum = 0;
+        //      for(let item of args){
+        //          numberssum = numberssum + item;
+        //      }
+        //      console.log(numberssum);
+        //  }
+        //  kate(10, 50, 6, 7, 8, 11, 6, 3, 9);
+
+
+    
+        //  ფუნქციამ უნდა დააბრუნოს სახელი და გვარი ადგილის გამოტოვებით (მაგ: 'giorgi saakadze')
+        //  თუ isloggedin არის true და false თუ isloggedin ფროფერთი არის false;
+
+        //  let user = {
+        //     firstname: 'giorgi',
+        //     lastname: 'saakadze',
+        //     age: 32,
+        //     isloggedin: true
+        //   }
+          
+         
+        //   function test(names) {
+        //       if (names.isloggedin) {
+        //           return names.firstname + " " + names.lastname;
+        //       }
+        //       return false;
+        //   }
+
+        //     let result = test(user);
+        //     console.log(result);
+
+
+
+
+
+        // 4. შექმენით ფუნქცია რომელსაც გადაეცემა ერთი პარამეტრი. 
+        // ეს პარამეტრი უნდა იყოს რიცხვების მასივი და ფუნქციამ უნდა დააბრუნოს ამ რიცხვებიდან მაქსიმალური:
+
+
+
+        function getMaxValue(numbers) {
+            let maxValue = 0;
+      
+            for (let i = 0; i < numbers.length; i++) {
+                let y = numbers[i];
+      
+                if (y > maxValue) {
+                    maxValue = y
+                } 
+                
+            }
+            return maxValue;
+        }
+        
+        console.log(getMaxValue([3, 102, 25, 110, 36]));
+
+
 
 
 
